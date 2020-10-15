210e1f516c9d9effabafae52cc9cfca806b3abca
John Alamina <john.alamina@gmail.com>
2020-10-15 15:06:07 +0100

    loancalc01

--- BasicSample/app/src/main/java/iscnotes/LoanCalc.java
+++ BasicSample/app/src/main/java/iscnotes/LoanCalc.java
@@ -1,6 +1,17 @@
-//package iscnotes
+package iscnotes;
+
 
 public class LoanCalc{
+
+    private int yrs;
+    private double rate;
+    private double amount;
+    
+    public LoanCalc(int y,double r, double a){
+      this.rate=r;
+      this.amt=a;
+    }
+
     public static void main(String a[]){
         System.out.println("hello isc");
     }
