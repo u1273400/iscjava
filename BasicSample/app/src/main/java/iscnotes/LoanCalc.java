c89e8c48b462676930e21bad9ee5c50648055ef3
John Alamina <john.alamina@gmail.com>
2020-10-17 06:28:01 +0100

    java07

--- BasicSample/app/src/main/java/iscnotes/LoanCalc.java
+++ BasicSample/app/src/main/java/iscnotes/LoanCalc.java
@@ -1,4 +1,4 @@
-package iscnotes;
+//package iscnotes;
 
 
 public class LoanCalc{
@@ -9,10 +9,22 @@ public class LoanCalc{
     
     public LoanCalc(int y,double r, double a){
       this.rate=r;
-      this.amt=a;
+      this.amount=a;
+      this.yrs=y;
     }
 
-    public static void main(String a[]){
-        System.out.println("hello isc");
+    public double ammortize() {
+        double r=rate/(100*12);
+        double n=yrs*12;
+        double a=amount;
+        double d = (Math.pow((1 + r),n)- 1)/ (r*Math.pow((1 + r),n));
+        return a/d ;
+    }
+
+    public static void main(String ar[]){
+        int y=7;int r=3;double a=10000;
+        LoanCalc carloan=new LoanCalc(y,r,a);
+        double p=carloan.ammortize();
+        System.out.println(String.format("monthly paymets on %3.2f for %dyrs at %d%% is %3.2f",a,y,r,p));
     }
 }
