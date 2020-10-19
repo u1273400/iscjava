48a5aa4fd1ab004c941d9d546353b4a97e0ae862
John Alamina <john.alamina@gmail.com>
2020-10-19 21:09:44 +0100

    nested

---
+++ BasicSample/app/src/main/java/iscnotes/Nested.java
@@ -0,0 +1,13 @@
+
+public class Nested{
+    public static void main (String[] a){
+        int A=5; //  A is going to be temperature in degrees C
+        if(A==0){
+            System.out.println( " Freezing! " ); //
+        }else if(A<0){
+            System.out.println( " Sub Zero " ); //
+        }else{
+            System.out.println( " Above zero " );
+        }
+    }
+}
