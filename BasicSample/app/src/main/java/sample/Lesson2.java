29b3fa9f5a21d30ab834f32a0e6c2d639ee69a52
john Alamina <john.alamina@hud.ac.uk>
2020-10-20 19:25:39 +0100

    textio

---
+++ BasicSample/app/src/main/java/sample/Lesson2.java
@@ -0,0 +1,21 @@
+package sample;
+
+import io.TextIO;
+
+public class Lesson2 {
+
+
+    public static void main(String args[]) {
+        //create object of the class
+        JavaInterfaceExample javaInterfaceExample = new JavaInterfaceExample();
+        //invoke sayHello(), declared in IntExample interface.
+        javaInterfaceExample.sayHello();
+    }
+
+
+}
+
+/*
+ OUTPUT of the above given Java Interface example would be :
+ Hello Visitor !
+ */
