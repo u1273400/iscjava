574a0d4f9a4c58d0d1a6996a078a8b99446d4786
John Alamina <john.alamina@gmail.com>
2020-10-23 12:13:52 +0100

    java10

--- BasicSample/app/src/main/java/iscnotes/LoanCalc.java
+++ BasicSample/app/src/main/java/iscnotes/LoanCalc.java
@@ -1,32 +1,34 @@
   //package iscnotes;
-
+import java.util.Scanner;
 
 public class LoanCalc{
 
-    private int yrs;
-    private double rate;
-    private double amount;
-    
-    public LoanCalc(int y,double r, double a){
-      this.rate=r;
-      this.amount=a;
-      this.yrs=y;
-    }
-    
-    
+  private int yrs;
+  private double rate;
+  private double amount;
+  
+  public LoanCalc(int y,double r, double a){
+    this.rate=r;
+    this.amount=a;
+    this.yrs=y;
+  }
+  
+  
 
-    public double ammortize() {
-        double r=rate/(100*12);
-        double n=yrs*12;
-        double a=amount;
-        double d = (Math.pow((1 + r),n)- 1)/ (r*Math.pow((1 + r),n));
-        return a/d ;
-    }
+  public double ammortize() {
+    double r=rate/(100*12);
+    double n=yrs*12;
+    double a=amount;
+    double d = (Math.pow((1 + r),n)- 1)/ (r*Math.pow((1 + r),n));
+    return a/d ;
+  }
 
-    public static void main(String ar[]){
-        int y=7;int r=3;double a=10000;
-        LoanCalc carloan=new LoanCalc(y,r,a);
-        double p=carloan.ammortize();
-        System.out.println(String.format("monthly paymets on %3.2f for %dyrs at %d%% is %3.2f",a,y,r,p));
-    }
+  public static void main(String ar[]){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter years=7:");
+    int y=TextIO.getlnInt();int r=3;double a=10000;
+    LoanCalc carloan=new LoanCalc(y,r,a);
+    double p=carloan.ammortize();
+    System.out.println(String.format("monthly payments on %3.2f for %dyrs at %d%% is %3.2f",a,y,r,p));
+  }
 }
