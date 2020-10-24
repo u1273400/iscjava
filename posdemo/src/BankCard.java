c4f3210bbd608b203ab5c28ee5ba3239f2f86350
John Alamina <john.alamina@gmail.com>
2020-10-24 01:18:07 +0100

    bank

--- posdemo/src/BankCard.java
+++ posdemo/src/BankCard.java
@@ -1,10 +1,28 @@
 public class BankCard {
     String pin="";
+    String cardid="";
+    Date expiry;
 
     public void setPin(String p){
         this.pin=p;
     }
     public String getPin() {
         return pin;
+    }
+
+    public Date getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(Date expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getCardid() {
+        return cardid;
+    }
+
+    public void setCardid(String cardid) {
+        this.cardid = cardid;
     }
 }
